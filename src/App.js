c4a48213cb585ed1517da30e73771b62a269fd04
Deena-D-Cutinha <deena.cutinha17@gmail.com>
2021-09-27 11:15:19 +0545

    added header,routers and components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Header from './Components/Header';
+import Home from './Components/Home';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      
+       <Router>
        <Header />
-     
+       <Switch>
+       <Route exact path="/">
+           <Home />
+          </Route>
+          <Route exact path="/home">
+           <Home />
+          </Route>
+          <Route exact path="/dashboard">
+           <Dashboard />
+          </Route>
+         
+        </Switch>
+        </Router>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
 import { Nav } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default class Header extends Component {
   render() {
     return (
       <div>
         <Nav defaultActiveKey="/home" as="ul">
           <Nav.Item as="li">
-            <Nav.Link href="/home">Active</Nav.Link>
+            <Nav.Link href="/home">Home</Nav.Link>
           </Nav.Item>
+          {/* <Nav.Item as="li">
+            <Nav.Link href="/home">LinkedIn</Nav.Link>
+          </Nav.Item> */}
           <Nav.Item as="li">
-            <Nav.Link eventKey="link-1">Link</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as="li">
-            <Nav.Link eventKey="link-2">Link</Nav.Link>
+            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
           </Nav.Item>
         </Nav>
       </div>
