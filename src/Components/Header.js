70ebc683e650ac65bb405dfc8d1906cbd057e362
Deena-D-Cutinha <deena.cutinha17@gmail.com>
2021-09-27 10:10:15 +0545

    initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Nav } from "react-bootstrap";
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Nav defaultActiveKey="/home" as="ul">
+          <Nav.Item as="li">
+            <Nav.Link href="/home">Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link eventKey="link-1">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
+    );
+  }
+}
